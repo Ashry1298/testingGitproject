99c239ff096b793262f514907dab50bf81316dd3
Ashry1298 <mostafaelashry36@email.com>
2023-09-14 16:09:58 +0300

    Mostafa Branch Edit

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,5 @@ Route::get('/', function () {
 });
 
 Route::get('/test', function () {
-    echo  'this is the first test for master branch  ';
+    echo  'this is the first test for mostafa branch  ';
 });
